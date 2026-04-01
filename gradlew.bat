e5a75dcd5a6fee9342d7b1e4e83e17565a074b88
lingyunzhe-blip <lingyunzhe@163.com>
2026-04-01 15:07:55 +0800

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+@"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
